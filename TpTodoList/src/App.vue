3c6c1076423c973d976353804cce794e22c691fa
agirard <agirard@datasolution.fr>
2022-08-13 22:34:01 +0200

    tutoriel todo list

---
+++ TpTodoList/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <todos v-model="todos"></todos>
+    <todos v-model="todos"></todos>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+export default {
+  data () {
+    return {
+      todos: [
+        {
+          name: 'Demo',
+          completed: true
+        }
+      ]
+    }
+  },
+  components: {
+    Todos
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        name: 'jean',
+        completed: false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
