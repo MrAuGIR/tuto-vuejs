d92a4a351aaf57e90a71de73748ea8a20508c541
agirard <agirard@datasolution.fr>
2022-08-13 14:46:34 +0200

    second chapitre - cycle de vie, method mounted et destroyed

--- js/App.js
+++ js/App.js
@@ -31,5 +31,31 @@ var list = new Vue({
     el:'#list',
     data: {
         persons: ['aurelien','julien','thomas']
+    },
+    methods: {
+        addPerson: function () {
+            this.persons.push('marion')
+        }
+    }
+})
+
+/**
+ * Les propriété traqué sont celles defini dans data
+ * syntax -> ajouter '$' devant les variables utilisé uniquement dans des fonctions
+ * @type {Vue}
+ */
+var vm = new Vue({
+    el: '#timer',
+    data:{
+        seconds: 0
+    },
+    mounted: function (){
+        this.$interval = setInterval(() => {
+            console.log('Time')
+            this.seconds ++
+        },1000)
+    },
+    destroyed: function () {
+        clearInterval(this.$interval)
     }
 })
